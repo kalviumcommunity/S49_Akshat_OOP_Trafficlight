d8225dd45eb0f11c6e7148ccef7dd9584c42db37
akshat33 <you@example.com>
2024-10-15 16:05:14 +0530

    added accessors and mutators

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 class TrafficLight {
     private:
-    string state;
-
-    static int totalTrafficLights;
+        string state;
+        static int totalTrafficLights;
 
     public:
         TrafficLight() : state("Red") {
@@ -16,52 +15,89 @@ class TrafficLight {
             cout << "Total Traffic Lights: " << totalTrafficLights << endl;
         }
 
-    void changeState() {
-        if (this->state == "Red") {
-            this->state = "Yellow";
-        } else if (this->state == "Yellow") {
-            this->state = "Green";
-        } else {
-            this->state = "Red";
+        string getState() const {
+            return state;
         }
-    }
 
-    string getState() const {
-        return this->state;
-    }
+        void setState(const string& newState) {
+            state = newState;
+        }
 
-    ~TrafficLight() {
-        --totalTrafficLights;  
-        cout << "Traffic Light with state " << state << " is being destroyed." << endl;
-        cout << "Total Traffic Lights: " << totalTrafficLights << endl;
-    }
+        void changeState() {
+            if (state == "Red") {
+                setState("Yellow");
+            } else if (state == "Yellow") {
+                setState("Green");
+            } else {
+                setState("Red");
+            }
+        }
 
-    static int getTotalTrafficLights() {
-        return totalTrafficLights;
-    }
+        ~TrafficLight() {
+            --totalTrafficLights;  
+            cout << "Traffic Light with state " << state << " is being destroyed." << endl;
+            cout << "Total Traffic Lights: " << totalTrafficLights << endl;
+        }
 
+        static int getTotalTrafficLights() {
+            return totalTrafficLights;
+        }
 };
 
 int TrafficLight::totalTrafficLights = 0;
 
+class Intersection {
+    private:
+        string name;
+        TrafficLight* trafficLights;
+        int numTrafficLights;
+
+    public:
+        Intersection(const string& name, int numLights) : name(name), numTrafficLights(numLights) {
+            trafficLights = new TrafficLight[numTrafficLights];
+        }
+
+        string getName() const {
+            return name;
+        }
+
+        void setName(const string& newName) {
+            name = newName;
+        }
+
+        int getNumTrafficLights() const {
+            return numTrafficLights;
+        }
+
+        void displayTrafficLights() const {
+            for (int i = 0; i < numTrafficLights; ++i) {
+                cout << "Traffic Light " << i + 1 << " at " << name << ": " << trafficLights[i].getState() << endl;
+            }
+        }
+
+        ~Intersection() {
+            delete[] trafficLights;
+            cout << "Intersection " << name << " is being destroyed." << endl;
+        }
+};
+
 int main() {
     const int numTrafficLights = 3;
 
-    TrafficLight* trafficLights = new TrafficLight[numTrafficLights];
+    Intersection intersection("Main Street", numTrafficLights);
+
+    intersection.displayTrafficLights();
 
     for (int i = 0; i < numTrafficLights; ++i) {
         for (int j = 0; j < 10; ++j) {
-            cout << "Traffic Light " << i + 1 << ": " << trafficLights[i].getState() << endl;
-            trafficLights[i].changeState();
+            intersection.displayTrafficLights();
         }
         cout << endl;
     }
 
     cout << "Total Traffic Lights after creation: " << TrafficLight::getTotalTrafficLights() << endl;
 
-    delete[] trafficLights;
-
-    cout << "Total Traffic Lights after deletion: " << TrafficLight::getTotalTrafficLights() << endl;
+    cout << "Total Traffic Lights after destruction: " << TrafficLight::getTotalTrafficLights() << endl;
 
     return 0;
 }
